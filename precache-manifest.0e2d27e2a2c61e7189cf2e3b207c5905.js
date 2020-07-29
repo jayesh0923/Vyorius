5a09fd4dfe3f57ea652c5a8f2d6b3ca1498dfecd
Jayesh Dhanrajani <jayeshdhanrajani09@gmail.com>
2020-07-29 19:47:37 +0530

    Updates

---
+++ precache-manifest.0e2d27e2a2c61e7189cf2e3b207c5905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f411a71d10bcdf2b2b0fa615433f67",
+    "url": "/Vyorius/index.html"
+  },
+  {
+    "revision": "27f27f439a8fcb1539f0",
+    "url": "/Vyorius/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "27f27f439a8fcb1539f0",
+    "url": "/Vyorius/static/js/2.97a532f8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Vyorius/static/js/2.97a532f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9812bc5806644ba51b68",
+    "url": "/Vyorius/static/js/main.816253c2.chunk.js"
+  },
+  {
+    "revision": "81a8e7b82c7a6ba65880",
+    "url": "/Vyorius/static/js/runtime-main.8bbb33d2.js"
+  },
+  {
+    "revision": "a00863f3fad5652bd9b355ed21c18022",
+    "url": "/Vyorius/static/media/Capture.a00863f3.png"
+  }
+]);
